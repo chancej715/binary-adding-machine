66e88b04b431ff93fd43f707e60975ebb0e22e18
Chance <chancej715@gmail.com>
2022-08-21 10:47:50 -0500

    Add two 8 bit numbers.

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ const halfAdder = (a, b) => {
     return `${sumOut}${carryOut}`
 }
 
+
 /**
  * Full adder
  */
@@ -68,5 +69,44 @@ const fullAdder = (a, b, cin) => {
     // Then we pass the first Carry Out, and the second Carry Out as inputs to the OR gate
     const orGateOutput = orGate(secondCarryOut, firstCarryOut)
 
+            // Sum Out     // Carry Out
     return `${secondSumOut}${orGateOutput}`
 }
+
+
+/**
+ * Add two 8 bit numbers
+ */
+const add = (a, b) => {
+    let sumOut = []
+
+    a = a.split('').reverse()
+    b = b.split('').reverse()
+    let cin = 0
+
+    for (let i = 0; i < 9; i++) {
+        const out = fullAdder(parseInt(a[i]), parseInt(b[i]), cin)
+
+        sumOut.push(out[0])
+        cin = parseInt(out[1])
+    }
+
+    return sumOut.reverse().join('')
+}
+
+
+/**
+ * Input two 8 bit numbers
+ */
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question('Enter first 8 bit binary number: ', first => {
+    readline.question('Enter second 8 bit binary number: ', second => {
+        console.log(add(first, second))
+
+        readline.close()
+    })
+})
